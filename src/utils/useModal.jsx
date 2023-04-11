856a39e2d056249388e8a8bacea5fdcacef7641c
ChuckBDT <charlesboudrant@protonmail.com>
2023-04-11 15:44:55 +0200

    custom hook modal added

---
+++ src/utils/useModal.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+function useModal() {
+  const [open, setOpen] = useState(false);
+
+  function setContent(content) {
+    if (open) {
+      return (
+        <div className='useModal-bg fixed top-0 z-10 h-screen w-screen bg-primary/50 overflow-hidden flex justify-center items-center'>
+          <div className='useModal-frame bg-tertiary p-8 m-4 rounded-2xl'>
+            <div
+              className='font-bold text-end cursor-pointer'
+              onClick={() => triggerModal()}
+            >
+              Close
+            </div>
+            <div className='useModal-content'>{content}</div>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  function triggerModal() {
+    setOpen(!open);
+  }
+
+  return [setContent, triggerModal];
+}
+
+export default useModal;
--- src/views/EmployeesList.jsx
+++ src/views/EmployeesList.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import useModal from "../utils/useModal";
 
 function EmployeesList() {
-  return <div className='bg-tertiary h-screen w-screen'>EmployeesList</div>;
+  const [setModalContentOne, setModalOpen] = useModal();
+
+  const content = (
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+      provident error sequi vitae praesentium natus mollitia tempora iusto sint?
+      Nostrum, odio. Porro odit repudiandae iusto cupiditate at laborum maxime
+      voluptas facere perferendis? Error sapiente animi dicta excepturi quam
+      eos? Quas expedita dicta cumque nulla deleniti inventore, necessitatibus
+      doloribus itaque ut.
+    </p>
+  );
+
+  return (
+    <>
+      {setModalContentOne(content)}
+      <button onClick={() => setModalOpen(12)} className='bg-red-500 h-24 w-48'>
+        Test
+      </button>
+    </>
+  );
 }
 
 export default EmployeesList;
